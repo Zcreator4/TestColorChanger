f87db1ef48e8792e57a7d6e5be41c11cccb1ae39
Zcreator4 <zander.jimenez@hgs.hiddengeniusproject.org>
2020-06-30 09:57:20 -0700

    first commit

---
+++ TEST/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;                 
+using System.Windows.Shapes;
+
+namespace TEST
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_1_Click(object sender, RoutedEventArgs e)
+        {
+            Button_2.Background = Brushes.Cyan;
+            button_3.Background = Brushes.Red;
+        }
+        
+        private void button_4_Click(object sender, RoutedEventArgs e)
+        {
+            button_3.Background = Brushes.White;
+            Button_2.Background = Brushes.White;
+        }
+    }
+}
